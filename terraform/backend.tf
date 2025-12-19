cb2acd01573571269768991465e8ceadc4b5ee5e
Aamani Kuntamukkala <aamani344@gmail.com>
2025-12-19 18:56:55 +0530

    Add Azure backend configuration for Terraform

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatehealthcare"
+    container_name       = "state"
+    key                  = "aks-dev.tfstate"
+  }
+}
