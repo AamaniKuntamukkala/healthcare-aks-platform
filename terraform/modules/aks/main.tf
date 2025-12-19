03c6a1c9fd965a4727ae4f144822461efdf358b9
Aamani Kuntamukkala <aamani344@gmail.com>
2025-12-19 20:27:48 +0530

    Add identity principal ID output to AKS module
    Added output for identity principal ID.

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -31,3 +31,6 @@ output "kubeconfig" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+output "identity_principal_id" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
