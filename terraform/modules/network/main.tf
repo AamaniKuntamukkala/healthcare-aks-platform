1c298706b3bbfff0b46104cd66d986b0c8b4d7eb
Aamani Kuntamukkala <aamani344@gmail.com>
2025-12-19 20:10:55 +0530

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,3 +1,6 @@
+variable "rg" {}
+variable "location" {}
+ 
 resource "azurerm_virtual_network" "vnet" {
   name                = "healthcare-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -7,7 +10,11 @@ resource "azurerm_virtual_network" "vnet" {
  
 resource "azurerm_subnet" "aks" {
   name                 = "aks-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.rg
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+ 
+output "subnet_id" {
+  value = azurerm_subnet.aks.id
 }
